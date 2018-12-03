eb830753fd3f793f220eb12f0f30bec397cf5417
Anum Tahir <anumtahir242@gmail.com>
2018-12-03 06:04:23 -0800

    Login

--- original/trytry/trytry/Controllers/HomeController.cs
+++ original/trytry/trytry/Controllers/HomeController.cs
@@ -6,12 +6,13 @@ using System.Web.Mvc;
 using System.Data;
 using System.Data.SqlClient;
 using trytry.Models;
+using System.Configuration;
 
 namespace trytry.Controllers
 {
     public class HomeController : Controller
     {
-        string connectionstring = @"Data Source=ABDULREHMAN;Initial Catalog=hotel;Integrated Security=True";
+        string connectionstring = @"Data Source=DESKTOP-1M6H1PO\ANUMSQL;Initial Catalog=hotel;Integrated Security=True";
         private object db;
         public ActionResult Index()
         {
@@ -94,6 +95,63 @@ namespace trytry.Controllers
                 }
 
             }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View(new Login());
+
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Login(Login loginmodel)
+        {
+            //string mainconn = ConfigurationManager.ConnectionStrings["hotelEntities2"].ConnectionString;
+            SqlConnection con = new SqlConnection(connectionstring);
+            con.Open();
+
+            string sqlquery = "select Email,Password from [dbo].[Sign_In] where Email=@Email and Password=@Password";
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, con);
+            sqlcomm.Parameters.AddWithValue("@Email", loginmodel.Email);
+            sqlcomm.Parameters.AddWithValue("@Password", loginmodel.Password);
+
+            //DataSet ds = new DataSet();
+            //SqlDataAdapter da = new SqlDataAdapter(sqlcomm);
+            //da.Fill(ds);
+            //con.Close();
+
+
+            //bool loginSuccessful = ((ds.Tables.Count > 0) && (ds.Tables[0].Rows.Count > 0));
+
+            //if (loginSuccessful)
+            //{
+            //    Console.WriteLine("Success!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid username or password");
+            //}
+
+
+
+
+
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+            if (sdr.Read())
+            {
+                Console.WriteLine("Success!");
+                return RedirectToAction("Cities");
+            }
+            else
+            {
+
+                Console.WriteLine("Invalid username or password");
+            }
+            con.Close();
+            return View();
+        }
 
     }
 }
